c046ca15b358aa6cbb7f5f534153e42693baacaf
BrunoMourão <73652232+Bruno27112000@users.noreply.github.com>
2022-10-11 11:13:56 -0300

    Feat(Ingredient-History) : Criei o Handler de criacao de novos itens no ingredientHistory

---
+++ Alpaca-Burguer-Api/Alpaca-Burguer.Application/IngredientHistory/AddRowIngredientHistoryRequest.cs
@@ -0,0 +1,13 @@
+﻿using Alpaca_Burguer.Business.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alpaca_Burguer.Application
+{
+    public class AddRowIngredientHistoryRequest : IRequest<IngredientHistory>
+    {
+        public IngredientHistory IngredientItem { get; set; }
+    }
+}
